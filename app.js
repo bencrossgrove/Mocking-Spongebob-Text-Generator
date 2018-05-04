81abbf7648deda950b4b8f5aeda2a76e7ac54052
Ben Crossgrove <ben.crossgrove@gmail.com>
2018-05-04 17:17:48 -0400

    typo fix

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get('/', function(req, res){
     });
 });
 
-app.listen(process.env.port || port, function(){
+app.listen(process.env.PORT || port, function(){
     console.log("Server started on port 3000");
 });
